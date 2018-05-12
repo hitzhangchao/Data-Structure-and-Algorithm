7838db9477d8ac3ad59bad7983c47b2dc00e2818
Zhang Chao <35788176+hitzhangchao@users.noreply.github.com>
2018-05-12 21:37:24 +0800

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	int asize = 6;
 	int a[] = { 34, 8, 64, 51, 32, 21 };
-	int i = 0,j = 0;
+	int i = 0, j = 0;
 	for (i = 1; i < asize; i++)    //从数组第二个值开始
 	{
 		int tmp = a[i];
@@ -14,9 +14,9 @@ int main()
 		{
 			a[j] = a[j - 1];
 		}
-		a[j] = tmp;
+		a[j] = tmp;  //这里的j会因为for再执行一次判断导致j-1了一次，故已经是正确的位置了，一定牢记！
 	}
-	
+
 	for (int k = 0; k < asize; k++)
 	{
 		cout << a[k] << " ";
