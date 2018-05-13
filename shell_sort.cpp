dd6724250ee2105ae83a7ef86c543d88cc6e4911
Zhang Chao <35788176+hitzhangchao@users.noreply.github.com>
2018-05-13 10:40:12 +0800

    Update shell_sort.cpp

--- shell_sort.cpp
+++ shell_sort.cpp
@@ -7,13 +7,13 @@ int main()
 	const int asize = 7;
 	int a[7] = { 34, 8, 64, 51, 32, 23, 21 };
 	int i, j, gap;
-	for (gap = asize / 2; gap > 0; gap /= 2)
+	for (gap = asize / 2; gap > 0; gap /= 2)  //设置增量hk
 	{
-		for (i = gap; i < asize; i++)
+		for (i = gap; i < asize; i++)   //从a[hk]处开始操作
 		{
 			int tmp = a[i];
 			j = i;
-			for (; j >= gap&&tmp < a[j - gap]; j -= gap)
+			for (; j >= gap&&tmp < a[j - gap]; j -= gap)  //比对交换
 			{
 				a[j] = a[j - gap];
 			}
